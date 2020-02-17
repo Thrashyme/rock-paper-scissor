6875ad064571047e9ffcfa106643ef6df9eb6b18
petra <petragrunheidt@gmail.com>
2020-02-17 09:45:50 -0300

    starting elements

---
+++ main.js
@@ -0,0 +1,11 @@
+function game(){ 
+function playRound(playerSelection, computerSelection) {
+switch(choice){
+    case rock
+}
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
+}
